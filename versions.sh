8ec0cb6216dd2617cd23c0211a54e5db65325811
Alex Skrypnyk <alex.designworks@gmail.com>
2019-08-09 13:43:52 +1000

    Added verions output.

---
+++ versions.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# Print version information for installed binaries.
+
+set -e
+
+DOCKER_IMAGE="${1}"
+
+commands=(
+  "git --version"
+  "ssh -V"
+  "lsof -v"
+  "zip --version"
+  "unzip -v"
+  "vim --version"
+  "lynx --version"
+  "curl --version"
+  "aspell --version"
+  "jq --version"
+  "shellcheck --version"
+  "docker --version"
+  "docker-compose --version"
+  "php --version"
+  "composer --version"
+  "npm --version"
+  "npx --version"
+  "node --version"
+  "goss --version"
+  "bats --version"
+  "ahoy --version"
+)
+
+for command in "${commands[@]}"; do
+  echo "----------------------------------------"
+  echo "${command%% *}"
+  echo "----------------------------------------"
+  # shellcheck disable=SC2086
+  docker run "${DOCKER_IMAGE}" ${command}
+  echo "----------------------------------------"
+  echo
+done;
